df04d2db22ea21531c5df404613f9bd24d5f860c
rupomroy1986 <roy.rupom6@gmail.com>
2020-03-05 10:49:00 +0530

    this is my 2nd commit

--- src/test/java/Academy/AppTest.java
+++ src/test/java/Academy/AppTest.java
@@ -1,38 +1,11 @@
 package Academy;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public static void main(String[] args)
+	{
+		System.out.println("helllo india");
+		System.out.println("helllo bagalore");
+		System.out.println("helllo USA");
+	}
 }
